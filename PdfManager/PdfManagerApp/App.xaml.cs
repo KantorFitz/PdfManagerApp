61bae9b8cee8e6d721e4c411747fa718f0f61f84
Krzysztof Jakubowski <fakedperson@o2.pl>
2023-10-22 17:44:40 +0200

    Initial app commit

---
+++ PdfManager/PdfManagerApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PdfManagerApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
