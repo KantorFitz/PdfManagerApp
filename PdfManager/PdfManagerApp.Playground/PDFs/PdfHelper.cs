0bacef5b33cc4c146c975279b9ceb9f33fa0ba61
Krzysztof Jakubowski <fakedperson@o2.pl>
2023-11-27 18:35:36 +0100

    More experiments

--- PdfManager/PdfManagerApp.Playground/PDFs/PdfHelper.cs
+++ PdfManager/PdfManagerApp.Playground/PDFs/PdfHelper.cs
@@ -3,4 +3,5 @@
 public static class PdfHelper
 {
     public static string GetSamplePdfPath() => @"PDFs/SamplePdf.pdf";
+    public static string GetSamplePdfPathWithImages() => @"PDFs/sample-pdf-with-images.pdf";
 }
--- PdfManager/PdfManagerApp.Playground/Program.cs
+++ PdfManager/PdfManagerApp.Playground/Program.cs
@@ -1,15 +1,27 @@
-﻿using MuPDFCore;
+﻿using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using iTextSharp.text;
+using MuPDFCore;
 using PDFManagerApp.PDFs;
 using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.Filters;
 
 namespace PDFManagerApp;
 
 public static class Program
 {
+    private const string outputFolder = "PdfOutputs";
+    
     public static void Main(string[] args)
     {
+        Directory.CreateDirectory(outputFolder);
+        
         // MuPdfImageExtractionExample();
-        PdfPigExample();
+        PdfPigExampleImages();
     }
 
     private static void PdfPigExample()
@@ -28,6 +40,53 @@ public static class Program
             
         }
     }
+
+    private static void PdfPigExampleImages()
+    {
+        using var document = PdfDocument.Open(PdfHelper.GetSamplePdfPathWithImages());
+
+        foreach (var page in document.GetPages())
+        {
+            foreach (var image in page.GetImages())
+            {
+                var imageType = string.Empty;
+
+                if (image.TryGetPng(out var byteArray))
+                {
+                    imageType = "png";
+                }
+                else if(image.TryGetBytes(out var ByteList))
+                {
+                    byteArray = ByteList.ToArray();
+                    imageType = "most likely JPG";
+                }
+                else
+                {
+                    byteArray = image.RawBytes.ToArray();
+                    imageType = "unknown";
+                }
+
+                File.WriteAllBytes(Path.Join(outputFolder, "test.png"), byteArray);
+
+            }
+            
+        }
+        
+        byte[] ApplyFilters(IPdfImage Image)
+        {
+            byte[] Result = Image.RawBytes.ToArray();
+            IReadOnlyList<IFilter> Filters = DefaultFilterProvider.Instance.GetFilters(Image.ImageDictionary);
+
+            foreach (var Filter in Filters)
+            {
+                if (Filter.IsSupported)
+                {
+                    Result = Filter.Decode(Result, Image.ImageDictionary, 0);
+                }
+            }
+            return Result;
+        }
+    }
     
     private static void MuPdfImageExtractionExample()
     {
@@ -39,6 +98,6 @@ public static class Program
 
         var zoomLevel = 1;
 
-        document.SaveImage(pageIndex, zoomLevel, PixelFormats.RGB, @"C:\Users\Kantor\Desktop\publish\output.png", RasterOutputFileTypes.PNG);
+        document.SaveImage(pageIndex, zoomLevel, PixelFormats.RGB, Path.Join(outputFolder, "test.png"), RasterOutputFileTypes.PNG);
     }
 }
