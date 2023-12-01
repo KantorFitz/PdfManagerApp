6a92c4a163db28822832e2f1c4b941a7efad60d7
Krzysztof Jakubowski <fakedperson@o2.pl>
2023-12-01 15:13:10 +0100

    Migration

---
+++ PdfManager/PdfManagerApp.Infrastructure/Migrations/20231201131357_InitialMigration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PdfManagerApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "folders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    AbsolutePath = table.Column<string>(type: "TEXT", nullable: false),
+                    PdfAmount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_folders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "book_details",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FolderId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FileName = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    NumberOfPages = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_book_details", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_book_details_folders_FolderId",
+                        column: x => x.FolderId,
+                        principalTable: "folders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_book_details_FolderId",
+                table: "book_details",
+                column: "FolderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "book_details");
+
+            migrationBuilder.DropTable(
+                name: "folders");
+        }
+    }
+}
