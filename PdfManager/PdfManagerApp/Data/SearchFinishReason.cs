fe1ebb38ec2c36f8a9708d480aaddecd16bcdedc
Krzysztof Jakubowski <fakedperson@o2.pl>
2023-12-07 12:54:09 +0100

    Add descriptions to SearchFinishReason enum

--- PdfManager/PdfManagerApp/Data/SearchFinishReason.cs
+++ PdfManager/PdfManagerApp/Data/SearchFinishReason.cs
@@ -1,9 +1,18 @@
-﻿namespace PdfManagerApp.Data;
+﻿using System.ComponentModel;
+
+namespace PdfManagerApp.Data;
 
 public enum SearchFinishReason
 {
+    [Description("Nieznany problem")]
     Unknown,
+
+    [Description("Wyszukiwanie zakończone")]
     FinishedNormally,
+
+    [Description("Anulowano")]
     OperationCancelled,
+
+    [Description("Wystąpił błąd")]
     ExceptionOccured,
 }
