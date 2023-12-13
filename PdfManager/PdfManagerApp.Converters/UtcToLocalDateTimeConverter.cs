2c3813b0d6b1615ae03b52b78e0de93ec501e978
Krzysztof Jakubowski <fakedperson@o2.pl>
2023-12-13 15:34:42 +0100

    Adde UTC to Local DateTime converter

---
+++ PdfManager/PdfManagerApp.Converters/UtcToLocalDateTimeConverter.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace PdfManagerApp.Converters;
+
+public class UtcToLocalDateTimeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (TryConvertToDateTime(value, out var dateTime))
+            return dateTime!.Value.ToLocalTime();
+
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (TryConvertToDateTime(value, out var dateTime))
+            return dateTime!.Value.ToUniversalTime();
+
+        return null;
+    }
+
+    private static bool TryConvertToDateTime(object value, out DateTime? dateTime)
+    {
+        if (value == null)
+        {
+            dateTime = null;
+            return false;
+        }
+
+        if (value is not DateTime dt)
+        {
+            dateTime = null;
+            return false;
+        }
+
+        dateTime = dt;
+        return true;
+    }
+}
